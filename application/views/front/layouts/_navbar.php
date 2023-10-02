ec6432790bab30216293f1051a4d9a99fe395f73
Raksibandu <raksibandusudarmanto257@gmail.com>
2023-10-02 01:42:11 -0700

    ubah navbar fix

--- application/views/front/layouts/_navbar.php
+++ application/views/front/layouts/_navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<div class="container">
 		<a class="navbar-brand" href="<?= base_url() ?>">
 			<img style="max-width:50px;" src="<?= base_url('img/identitas/logo3.jpg') ?>">
